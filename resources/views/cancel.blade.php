f94ba1081a613702d82890aea091a4697d248b04
andv <andv@vietis.com.vn>
2022-04-15 14:59:29 +0700

    push-code

---
+++ resources/views/cancel.blade.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=Shift_JIS"/>
+    <title>ﾏｰﾁｬﾝﾄ購入ｷｬﾝｾﾙ</title>
+</head>
+<body>
+購入ｷｬﾝｾﾙ<br>
+■■■マーチャント受取パラメータ■■■<br><br>
+<table border='1'>
+</table>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::post('/payment/call_back_function', function () {
+    return json_encode('OK;');
+});
+
+Route::post('/payment/success', function () {
+    echo 'success';
+});
+
+Route::post('/payment/cancel', function () {
+    return redirect()->route('cancel');
+});
+
+Route::get('/alert_cancel', function () {
+    return view('cancel');
+})->name('cancel');
+
+Route::post('/payment/error', function () {
+    echo 'error';
+});
