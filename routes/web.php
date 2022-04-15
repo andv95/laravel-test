f2062fcb4d9de590f50815bdd90f312f8d44dce5
andv <andv@vietis.com.vn>
2022-04-15 15:26:37 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::post('/payment/call_back_function', function () {
     return json_encode('OK;');
